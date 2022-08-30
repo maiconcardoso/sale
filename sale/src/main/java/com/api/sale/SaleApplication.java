187476185ba91efe80ea167eb4e5cac79a1e5596
maicon_cardoso <maiconscardoso@hotmail.com>
2022-08-30 20:43:43 -0300

    Criando um método para inserir uma entidade teste no banco de dados

--- sale/src/main/java/com/api/sale/SaleApplication.java
+++ sale/src/main/java/com/api/sale/SaleApplication.java
@@ -1,10 +1,24 @@
 package com.api.sale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.api.sale.entities.Client;
+import com.api.sale.repositories.ClientRepository;
 
 @SpringBootApplication
 public class SaleApplication {
+
+	@Bean
+	private CommandLineRunner run(@Autowired ClientRepository clientRepository) {
+		return args -> {
+			Client client = Client.builder().cpf("00000000000").name("Fulano de tal").build();
+			clientRepository.save(client);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaleApplication.class, args);
