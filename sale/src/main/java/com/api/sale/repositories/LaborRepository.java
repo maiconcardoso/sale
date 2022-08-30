e25455e374a765dbd387c89b9448739b931a9f02
maicon_cardoso <maiconscardoso@hotmail.com>
2022-08-30 20:11:24 -0300

    Criando as interfaces para repositorio

---
+++ sale/src/main/java/com/api/sale/repositories/LaborRepository.java
@@ -0,0 +1,11 @@
+package com.api.sale.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.sale.entities.Labor;
+
+@Repository
+public interface LaborRepository extends JpaRepository<Labor, Integer>{
+    
+}
