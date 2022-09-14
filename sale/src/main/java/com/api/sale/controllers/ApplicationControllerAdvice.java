5a9d08c0e414cba8b80fc99430b296006457bfae
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-14 06:49:00 -0300

    Finalização de mensagens de erro

--- sale/src/main/java/com/api/sale/controllers/ApplicationControllerAdvice.java
+++ sale/src/main/java/com/api/sale/controllers/ApplicationControllerAdvice.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.api.sale.exceptions.ApiErrors;
 
@@ -25,5 +27,13 @@ public class ApplicationControllerAdvice {
                         .collect(Collectors.toList());
         
         return new ApiErrors(messages);
+    }
+
+    @ExceptionHandler (ResponseStatusException.class)
+    public ResponseEntity<ApiErrors> handleResponseStatusException(ResponseStatusException ex) {
+        String messageError = ex.getMessage();
+        HttpStatus httpStatus = ex.getStatus();
+        ApiErrors api = new ApiErrors(messageError);
+        return new ResponseEntity<ApiErrors>(api, httpStatus);
     }
 }
