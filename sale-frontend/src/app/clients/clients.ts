eb476f6d23f204c41606eef08474b74a3f19179f
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-19 20:55:52 -0300

    Modificação da classe client

--- sale-frontend/src/app/clients/clients.ts
+++ sale-frontend/src/app/clients/clients.ts
@@ -1,13 +1,7 @@
 export class Client {
-    id: number;
-    name: string;
-    cpf: string;
-    registaryDate: string;
+    id?: number;
+    name: string = '';
+    cpf: string = '';
+    registaryDate: string = '';
 
-    constructor(id: number, name: string, cpf: string, registaryDate: string) {
-        this.id = id;
-        this.name = name;
-        this.cpf = cpf;
-        this.registaryDate = registaryDate;
-    }
 }
