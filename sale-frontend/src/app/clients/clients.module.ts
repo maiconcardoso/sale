149cb9f4437141e96dc3a3d748ccad996f286d26
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-21 20:04:33 -0300

    Inserção do componente ClientsListComponent no module

--- sale-frontend/src/app/clients/clients.module.ts
+++ sale-frontend/src/app/clients/clients.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsFormComponent } from './clients-form/clients-form.component';
 import { FormsModule } from '@angular/forms';
+import { ClientsListComponent } from './clients-list/clients-list.component';
 
 
 
 
 @NgModule({
   declarations: [
-    ClientsFormComponent
+    ClientsFormComponent,
+    ClientsListComponent
   ],
   imports: [
     CommonModule,
