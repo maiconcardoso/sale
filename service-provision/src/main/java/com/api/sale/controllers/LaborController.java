d69e4667060fec01d3a68e4bcd23bed7c1cad222
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-25 12:24:34 -0300

    Criação do camada controladora de labor

---
+++ service-provision/src/main/java/com/api/sale/controllers/LaborController.java
@@ -0,0 +1,27 @@
+package com.api.sale.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.api.sale.dtos.LaborDto;
+import com.api.sale.entities.Labor;
+import com.api.sale.services.LaborService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("api/labor")
+public class LaborController {
+    
+    private final LaborService laborService;
+
+    @PostMapping
+    public ResponseEntity<Labor> save(@RequestBody LaborDto laborDto) {
+        return new ResponseEntity<>( laborService.save(laborDto), HttpStatus.OK);
+    }
+}
