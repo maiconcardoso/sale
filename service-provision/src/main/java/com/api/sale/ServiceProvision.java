37aebab2c3104bfe658a78cccdaf44ecaa56046b
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-25 12:25:17 -0300

    Adicionando um serviço no banco de dados em memória

--- service-provision/src/main/java/com/api/sale/ServiceProvision.java
+++ service-provision/src/main/java/com/api/sale/ServiceProvision.java
@@ -1,21 +1,32 @@
 package com.api.sale;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.api.sale.entities.Client;
+import com.api.sale.entities.Labor;
 import com.api.sale.repositories.ClientRepository;
+import com.api.sale.repositories.LaborRepository;
 
 @SpringBootApplication
 public class ServiceProvision {
 
 	@Bean
-	CommandLineRunner run(ClientRepository clientRepository) {
+	CommandLineRunner run(ClientRepository clientRepository, LaborRepository laborRepository) {
 		return args -> {
 			Client client = Client.builder().cpf("90821388088").name("Fulano de tal").build();
 			clientRepository.save(client);
+
+			Labor labor = Labor.builder().client(client).description("Formatação de PC").date(LocalDate.now())
+					.amount(34.90)
+					.build();
+			laborRepository.save(labor);
+
 		};
 	}
 
