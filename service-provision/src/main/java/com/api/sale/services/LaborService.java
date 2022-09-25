c7bbf1721452d68b3c7d21087de1bb5994ff48fa
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-25 12:28:34 -0300

    Criação do método save e eliminação do conversor BigDecimal

--- service-provision/src/main/java/com/api/sale/services/LaborService.java
+++ service-provision/src/main/java/com/api/sale/services/LaborService.java
@@ -4,9 +4,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.api.sale.dtos.LaborDto;
+import com.api.sale.entities.Client;
 import com.api.sale.entities.Labor;
 import com.api.sale.repositories.LaborRepository;
-import com.api.sale.utils.BigDecimalConverter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,13 +15,13 @@ import lombok.RequiredArgsConstructor;
 public class LaborService {
     
     private final LaborRepository repository;
-    private final BigDecimalConverter bigDecimalConverter;
-
+    private final ClientService clientService;
 
     public Labor save(LaborDto laborDto) {
         var labor = new Labor();
-        bigDecimalConverter.converter(laborDto.getAmount().toString());
         BeanUtils.copyProperties(laborDto, labor);
+        Client client = clientService.findById(laborDto.getIdClient());
+        labor.setClient(client);
         return repository.save(labor);
     } 
 }
