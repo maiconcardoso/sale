97bee03d3355101b3db3af7a1baf3258ad2290b7
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-25 22:53:28 -0300

    Modificação do tipo para o atributo date

--- service-provision/src/main/java/com/api/sale/dtos/LaborDto.java
+++ service-provision/src/main/java/com/api/sale/dtos/LaborDto.java
@@ -1,11 +1,7 @@
 package com.api.sale.dtos;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.time.LocalDate;
-
-import com.api.sale.entities.Client;
-import com.api.sale.entities.Labor;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,6 +15,6 @@ public class LaborDto implements Serializable{
     private String description;
     private Integer idClient;
     private Double amount;
-    private LocalDate date;
+    private String date;
     
 }
