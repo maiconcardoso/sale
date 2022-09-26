b1a13259ed27041f7fcc45f7ea36b7491cc14a28
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-25 22:54:48 -0300

    Alteração do método save para manipulação do formato da data

--- service-provision/src/main/java/com/api/sale/services/LaborService.java
+++ service-provision/src/main/java/com/api/sale/services/LaborService.java
@@ -1,5 +1,8 @@
 package com.api.sale.services;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +21,12 @@ public class LaborService {
     private final ClientService clientService;
 
     public Labor save(LaborDto laborDto) {
+        LocalDate date = LocalDate.parse(laborDto.getDate(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         var labor = new Labor();
         BeanUtils.copyProperties(laborDto, labor);
         Client client = clientService.findById(laborDto.getIdClient());
         labor.setClient(client);
+        labor.setDate(date);
         return repository.save(labor);
     } 
 }
