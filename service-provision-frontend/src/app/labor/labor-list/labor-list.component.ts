b475171de2e35ed49408e187f69acc1fd79e90e0
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-29 06:45:06 -0300

    Crianção do formulário para pesquisa de serviços prestados por cliente

--- service-provision-frontend/src/app/labor/labor-list/labor-list.component.ts
+++ service-provision-frontend/src/app/labor/labor-list/labor-list.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaborListComponent implements OnInit {
 
-  constructor() { }
+  clientName: string;
+  month: number;
+  months: number[];
+
+  constructor() { 
+    this.months = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12];
+  }
 
   ngOnInit(): void {
+  }
+
+  searchClient() {
+    console.log(this.clientName, this.month);
   }
 
 }
