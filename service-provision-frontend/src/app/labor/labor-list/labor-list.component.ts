8e4f03744dbe01ccb1cf05a881cf801feea2cb2b
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-29 20:22:02 -0300

    Criação de tabela de pesquisa de serviços prestados e botões de retorno e consulta

--- service-provision-frontend/src/app/labor/labor-list/labor-list.component.ts
+++ service-provision-frontend/src/app/labor/labor-list/labor-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LaborService } from 'src/app/services/labor.service';
+import { LaborProvision } from './labor-provision';
 
 @Component({
   selector: 'app-labor-list',
@@ -10,16 +12,25 @@ export class LaborListComponent implements OnInit {
   clientName: string;
   month: number;
   months: number[];
+  laborList: LaborProvision[] = [];
+  message: any;
 
-  constructor() { 
-    this.months = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12];
+  constructor(private laborService: LaborService) { 
+    this.months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   }
 
   ngOnInit(): void {
   }
 
   searchClient() {
-    console.log(this.clientName, this.month);
+    this.laborService.searchLaborByClientName(this.clientName, this.month).subscribe(response => {
+      this.laborList = response;
+      if (this.laborList.length <= 0) {
+        this.message = "Elementos não encontrados.";
+      } else {
+        this.message = null;
+      }
+    })
   }
 
 }
--- service-provision-frontend/src/app/services/labor.service.ts
+++ service-provision-frontend/src/app/services/labor.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Client } from '../clients/clients';
 import { Labor } from '../labor/labor';
+import { LaborProvision } from '../labor/labor-list/labor-provision';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,10 @@ export class LaborService {
     return this.http.post<Labor>(`${this.baseURL}`, labor);
   }
 
-  public searchLaborByClientName(client: Client) : Observable<Labor> {
-    const url: string = `${this.baseURL}?name=${client.name}`
-    return this.http.get<Labor>(url);
+  public searchLaborByClientName(name: string, month: number) : Observable<LaborProvision[]> {
+    let httpParams = new HttpParams().set('name', name).set('month', month ? month.toString() : '' );
+    const url = `${this.baseURL}?${httpParams}`
+    console.log(url)
+    return this.http.get<LaborProvision[]>(url);
   }
 }
