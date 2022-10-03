aaa2522032a3ad2a4d06a1bfd2dd93289fe2deb4
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-02 21:39:40 -0300

    Eliminação de imports não mais utilizados

--- service-provision/src/main/java/com/api/sale/entities/Labor.java
+++ service-provision/src/main/java/com/api/sale/entities/Labor.java
@@ -9,9 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.PrePersist;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
