4d51814c7934c365e1d734a45b808fc498fe9c77
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-05 06:58:49 -0300

    Modificação de string para melhoria na visualização

--- service-provision/src/main/java/com/api/sale/exceptions/UsernameRegisteredException.java
+++ service-provision/src/main/java/com/api/sale/exceptions/UsernameRegisteredException.java
@@ -3,6 +3,6 @@ package com.api.sale.exceptions;
 public class UsernameRegisteredException extends RuntimeException{
     
     public UsernameRegisteredException(String login) {
-        super("User already registered to login" + login);
+        super("User already registered to username " + login);
     }
 }
