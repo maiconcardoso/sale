6977da59228bc286fafa05e1721371427b70dd65
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-04 22:14:49 -0300

    Criação de bean e modificação de método para receber um JWT

--- service-provision/src/main/java/com/api/sale/config/AuthorizationServerConfig.java
+++ service-provision/src/main/java/com/api/sale/config/AuthorizationServerConfig.java
@@ -1,5 +1,6 @@
 package com.api.sale.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,8 +8,11 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,15 +23,26 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     
     private final AuthenticationManager authenticationManager;
 
+    @Value("${security.jwt.signing-key}")
+    private String signingKey;
+
     @Bean
     public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey(signingKey);
+        return accessTokenConverter;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception{
         endpoints
             .tokenStore(tokenStore())
+            .accessTokenConverter(accessTokenConverter())
             .authenticationManager(authenticationManager);
     }
 
