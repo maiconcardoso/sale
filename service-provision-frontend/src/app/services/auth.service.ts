31044fb9e109999b8c0b74165127758f546f717d
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-05 06:51:00 -0300

    Desenvolvimento da função de autenticação de usuário

---
+++ service-provision-frontend/src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  baseUrl: string = environment.URL;
+  urlToken: string = this.baseUrl + environment.urlToken;
+  clientId: string = environment.clientId;
+  clientSecret: string = environment.clientSecret;
+
+  constructor(private http: HttpClient) { }
+
+  public login(username: string, password: string): Observable<any> {
+    const params = new HttpParams()
+      .set('username', username)
+      .set('password', password)
+      .set('grant_type', 'password')
+
+    const headers = {
+      'Authorization': 'Basic ' + btoa(`${this.clientId}:${this.clientSecret}`),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    return this.http.post(this.urlToken, params.toString(), { headers } );
+  }
+}
