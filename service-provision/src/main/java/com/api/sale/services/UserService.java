214af3a52c52c6fe3b98006c7aeba5c80e29d9fb
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-04 22:18:17 -0300

    Modificação de método save para saber se usuário já está cadastrado, caso esteja lança uma exceção

--- service-provision/src/main/java/com/api/sale/services/UserService.java
+++ service-provision/src/main/java/com/api/sale/services/UserService.java
@@ -3,6 +3,7 @@ package com.api.sale.services;
 import org.springframework.stereotype.Service;
 
 import com.api.sale.entities.User;
+import com.api.sale.exceptions.UsernameRegisteredException;
 import com.api.sale.repositories.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,10 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User save(User user) {
+        boolean existsUsername = userRepository.existsByUsername(user.getUsername());
+        if (existsUsername) {
+            throw new UsernameRegisteredException(user.getUsername());
+        }
         return userRepository.save(user);
     }
 
