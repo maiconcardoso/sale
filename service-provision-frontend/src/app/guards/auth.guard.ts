2118b8619e31559ad758bc12e68a67fc1ffb98bb
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-07 06:48:05 -0300

    Inserção de router para usuário não autenticado

--- service-provision-frontend/src/app/guards/auth.guard.ts
+++ service-provision-frontend/src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
@@ -9,13 +9,15 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService, private router: Router
   ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-    return this.authService.isAuthenticated();
+    const authenticated = this.authService.isAuthenticated();
+    if (authenticated) return true;
+    else this.router.navigate(['/login']); return false;
   }
   
 }
