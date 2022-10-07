f5a17f5a7430247638dd61dbe1b3d07b488fdb17
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-06 21:19:24 -0300

    Inserção de código para verificação de expiração de token na aplicação

--- service-provision-frontend/src/app/services/auth.service.ts
+++ service-provision-frontend/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -12,8 +13,27 @@ export class AuthService {
   urlToken: string = this.baseUrl + environment.urlToken;
   clientId: string = environment.clientId;
   clientSecret: string = environment.clientSecret;
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
+
+  public getToken() {
+    const tokenString = localStorage.getItem('access_token');
+    if (tokenString) {
+      const token = JSON.parse(tokenString).access_token;
+      return token;
+    }
+    return null;
+  }
+
+  public isAuthenticated(): boolean {
+    const token = this.getToken();
+    if (token) {
+      const expired = !(this.jwtHelper.isTokenExpired(token));
+      return expired;
+    }
+    return false;
+  }
 
   public login(username: string, password: string): Observable<any> {
     const params = new HttpParams()
