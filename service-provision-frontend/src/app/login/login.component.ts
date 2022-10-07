048275a05e8f7456d9ca01f44ee87feaa2040eaa
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-06 21:18:41 -0300

    Inserção de código de captura de token no método onSubmit()

--- service-provision-frontend/src/app/login/login.component.ts
+++ service-provision-frontend/src/app/login/login.component.ts
@@ -18,6 +18,8 @@ export class LoginComponent {
 
   onSubimit(){
     this.auth.login(this.username, this.password).subscribe(response => {
+      const access_token = JSON.stringify(response);
+      localStorage.setItem('access_token', access_token);
       this.router.navigate(['/home'])
     }, errorResponse => {
       this.errors = errorResponse.error.errors;
